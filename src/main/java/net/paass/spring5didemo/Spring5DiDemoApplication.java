876039269a72b83bedd6a7feec602e97fe2b7a95
Sven Paaß <sven@paass.net>
2019-03-31 09:18:46 +0200

    simple dependency injection

--- src/main/java/net/paass/spring5didemo/Spring5DiDemoApplication.java
+++ src/main/java/net/paass/spring5didemo/Spring5DiDemoApplication.java
@@ -1,13 +1,19 @@
 package net.paass.spring5didemo;
 
+import net.paass.spring5didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring5DiDemoApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(Spring5DiDemoApplication.class, args);
+    ApplicationContext ctx = SpringApplication.run(Spring5DiDemoApplication.class, args);
+
+    // manually get bean from context
+    MyController controller = (MyController) ctx.getBean("myController");
+    controller.sayHello();
   }
 
 }
---
+++ src/main/java/net/paass/spring5didemo/controllers/MyController.java
@@ -0,0 +1,12 @@
+package net.paass.spring5didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+  public void sayHello() {
+    System.out.println("Hello!");
+  }
+
+}
