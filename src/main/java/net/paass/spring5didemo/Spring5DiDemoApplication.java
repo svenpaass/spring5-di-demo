dbd251a2daf791b5f2cfd5fbf33dad4a6002f3a3
Sven Paaß <sven@paass.net>
2019-03-31 08:57:13 +0200

    Initial Commit

---
+++ src/main/java/net/paass/spring5didemo/Spring5DiDemoApplication.java
@@ -0,0 +1,13 @@
+package net.paass.spring5didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5DiDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Spring5DiDemoApplication.class, args);
+  }
+
+}
