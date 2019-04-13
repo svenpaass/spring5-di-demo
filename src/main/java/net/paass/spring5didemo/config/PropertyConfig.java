9c7df7f3c3690ad2999bc52b64468bb90c5dfefe
Sven Paaß <sven@paass.net>
2019-04-13 13:52:11 +0200

    application.properties
    will be pulled in by default.

--- src/main/java/net/paass/spring5didemo/config/PropertyConfig.java
+++ src/main/java/net/paass/spring5didemo/config/PropertyConfig.java
@@ -2,25 +2,12 @@ package net.paass.spring5didemo.config;
 
 import net.paass.spring5didemo.examplebeans.FakeDataSource;
 import net.paass.spring5didemo.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({ "classpath:datasource.properties", "classpath:jms.properties" })
-@PropertySources({
-    @PropertySource("classpath:datasource.properties"),
-    @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-  @Autowired
-  Environment env;
 
   @Value("${database.username}")
   String user;
@@ -44,12 +31,7 @@ public class PropertyConfig {
   @Bean
   public FakeDataSource fakeDataSource() {
     FakeDataSource fakeDataSource = new FakeDataSource();
-
-    if ( env.getProperty("USERNAME") != null ) {
-      fakeDataSource.setUser(env.getProperty("USERNAME"));
-    } else {
-      fakeDataSource.setUser(user);
-    }
+    fakeDataSource.setUser(user);
     fakeDataSource.setPassword(password);
     fakeDataSource.setUrl(url);
     return fakeDataSource;
@@ -62,12 +44,6 @@ public class PropertyConfig {
     fakeJmsBroker.setPassword(jmsPassword);
     fakeJmsBroker.setUrl(jmsUrl);
     return fakeJmsBroker;
-  }
-
-  @Bean
-  public static PropertySourcesPlaceholderConfigurer properties() {
-    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-    return propertySourcesPlaceholderConfigurer;
   }
 
 }
